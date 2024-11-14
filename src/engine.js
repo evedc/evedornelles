58bb2e0924a05e9fee88bb77719d16b0215eb714
Évelim Dornelles <dorrrnelles@gmail.com>
2024-11-14 11:11:58 -0300

    upload

--- src/engine.js
+++ src/engine.js
@@ -1,12 +1,12 @@
 
-
 window.addEventListener("scroll", function () {
     var imagem = document.getElementById("imagem");
     var posicaoImagem = imagem.getBoundingClientRect().top;
     var alturaTela = window.innerHeight;
 
-    // Ativa a animação quando a imagem está na tela
+
     if (posicaoImagem < alturaTela) {
         imagem.classList.add("ativa");
     }
 });
+
