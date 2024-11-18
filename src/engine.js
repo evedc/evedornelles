e5152d4a837d9c833e28dba0c9129fc88f74cdcd
Évelim Dornelles <dorrrnelles@gmail.com>
2024-11-18 08:55:07 -0300

    upload

--- src/engine.js
+++ src/engine.js
@@ -1,29 +1,28 @@
 window.addEventListener("scroll", function () {
-    var imagens = document.querySelectorAll(".imagem"); // Seleciona todas as imagens com a classe .imagem
+    var imagens = document.querySelectorAll(".imagem"); 
 
     imagens.forEach(function (imagem) {
         var posicaoImagem = imagem.getBoundingClientRect().top;
         var alturaTela = window.innerHeight;
 
-        // Ativa a animação quando a imagem está na tela
+       
         if (posicaoImagem < alturaTela && posicaoImagem > 0) {
             imagem.classList.add("ativa");
         } else {
-            imagem.classList.remove("ativa"); // Remove a animação ao sair da tela para repetição
+            imagem.classList.remove("ativa"); 
         }
     });
 });
 
 window.addEventListener("scroll", function () {
-    var skillsGrid = document.querySelector(".skills"); // Seleciona o grid de skills
+    var skillsGrid = document.querySelector(".skills"); 
 
     var posicaoGrid = skillsGrid.getBoundingClientRect().top;
     var alturaTela = window.innerHeight;
-
-    // Ativa a animação quando o grid está na tela
+    
     if (posicaoGrid < alturaTela && posicaoGrid > 0) {
         skillsGrid.classList.add("ativa");
     } else {
-        skillsGrid.classList.remove("ativa"); // Remove a animação ao sair da tela para repetição
+        skillsGrid.classList.remove("ativa");
     }
 });
